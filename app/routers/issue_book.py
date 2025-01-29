ec3bfd1b314d0580a8b52abba1afd6a7e9ddadd1
MadhavGupta1506 <gmadhav857@gmail.com>
2025-01-29 13:37:30 +0530

    requirements done

---
+++ app/routers/issue_book.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, HTTPException, Depends, APIRouter, status
+from sqlalchemy.orm import Session
+from sqlalchemy import and_
+from .. import schemas, models, database, utils, oauth2
+from datetime import date
+
+# Initialize the API router for book issue and return functionality
+router = APIRouter(prefix="/book/issuebook", tags=["Issue/Return Book"])
+
+@router.get("/")
+def issue():
+    # Endpoint to indicate the issue book functionality
+    return {"message": "Issue book"}
+
+@router.post("/issue_book", status_code=status.HTTP_201_CREATED)
+def issue_book(user_details: schemas.BookId, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user), role: str = Depends(oauth2.require_role(["librarian"]))):
+    # Issue a book to a user after checking availability
+    available = db.query(models.Book).filter(and_(models.Book.book_id == user_details.book_id, models.Book.status == "available")).first()
+    if not available:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Book with book_id {user_details.book_id} is already issued and unavailable currently!")
+
+    available.status = "issued"
+    user = db.query(models.Users).filter(models.Users.uid == current_user.uid).first()
+
+    issue = models.IssueBook(
+        book_id=user_details.book_id,
+        issue_date=date.today(),
+        return_date=utils.get_return_date(date.today()),
+        uid=current_user.uid,
+        user_name=user.user_name,
+        email=user.email,
+        mobile=user.mobile
+    )
+
+    db.add(issue)
+    db.add(available)
+    db.commit()
+    db.refresh(available)
+
+    return available
+
+@router.post("/return_book/{book_id}")
+def return_book(book_id: int, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user), role: str = Depends(oauth2.require_role(["librarian"]))):
+    # Return a book after checking if it was issued
+    issued = db.query(models.IssueBook).filter(models.IssueBook.book_id == book_id).first()
+
+    if not issued:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book is not issued yet!")
+    book_status = db.query(models.Book).filter(models.Book.book_id == book_id).first()
+    if not book_status:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with id {book_id} not found!")
+
+    if current_user.uid != issued.uid:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Not Authorized!")
+    book_status.status = "available"
+    db.delete(issued)
+    db.commit()
+
+    db.refresh(book_status)
+    return book_status
---
+++ app/routers/login.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, HTTPException, Depends, status, Response, APIRouter
+from sqlalchemy.orm import Session
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from .. import database, models, schemas, utils, oauth2
+
+# Initialize the API router for login functionality
+router = APIRouter(prefix="/login", tags=["Login"])
+
+@router.post("/", response_model=schemas.Token)
+def login(credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    # Authenticate user and return a JWT token
+    user = db.query(models.Users).filter(models.Users.email == credentials.username).first()
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials!!")
+    
+    if not utils.verify_password(credentials.password, user.password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials!!")
+    
+    token = oauth2.create_access_token(data={"user_id": user.uid})
+    return schemas.Token(token=token, token_type="bearer")
